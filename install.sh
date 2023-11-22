4bc874bec99ade167bce7139faab1c426c2cc8f3
Luishiño <pericena12@gmail.com>
2023-11-22 04:56:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Comentarios descriptivos
-# Este script instala y configura Nextcloud en un servidor Ubuntu.
+# Este script instala y configura Nextcloud en un servidor Ubuntu con Apache2.
+# También incluye la creación de un certificado SSL.
 
 # Solicitar la contraseña para el usuario de Nextcloud
 read -s -p "Ingresa la contraseña para el usuario de Nextcloud: " nextcloud_user_password
@@ -12,19 +12,28 @@ read -s -p "Ingresa la contraseña para la base de datos de Nextcloud: " db_pass
 echo  # Agregar nueva línea después de la entrada de contraseña
 
 # Validar entrada del usuario
-if [ -z "$nextcloud_user_password" ] || [ -z "$db_password" ]; then
-    echo "Las contraseñas no pueden estar en blanco. Salir."
+if [[ -z "$nextcloud_user_password" || -z "$db_password" ]]; then
+    echo "Las contraseñas no pueden estar en blanco. Saliendo."
     exit 1
 fi
+
+# Crear directorio para los certificados
+sudo mkdir -p /etc/apache2/ssl
+cd /etc/apache2/ssl
+
+# Generar claves SSL
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout nextcloud.key -out nextcloud.crt -subj "/C=US/ST=State/L=City/O=Organization/OU=Unit/CN=agoranube.ddns.net"
+
+# Ajustar permisos
+sudo chmod 600 nextcloud.key
+sudo chmod 644 nextcloud.crt
 
 # Actualizar el sistema
 sudo apt update && sudo apt upgrade -y
 
 # Verificar la actualización del sistema
-if [ $? -eq 0 ]; then
-    echo "Sistema actualizado con éxito"
-else
-    echo "Error al actualizar el sistema. Salir."
+if [ $? -ne 0 ]; then
+    echo "Error al actualizar el sistema. Saliendo."
     exit 1
 fi
 
@@ -35,24 +44,41 @@ sudo ufw allow 80  # Permitir tráfico HTTP
 sudo ufw allow 443 # Permitir tráfico HTTPS
 sudo ufw enable
 
-# Instalar Apache2
-sudo apt install -y apache2
+# Instalar Apache2 y módulos SSL
+sudo apt install -y apache2 libapache2-mod-php
 
-# Instalar certbot y obtener el certificado SSL
-sudo apt install -y certbot python3-certbot-apache
-sudo certbot --apache -d agoranube.ddns.net --non-interactive --agree-tos --email tu@email.com
+# Habilitar módulos
+sudo a2enmod ssl
+sudo a2enmod headers
+sudo a2enmod rewrite
 
-# Verificar la instalación del certificado SSL
-certbot_status=$?
-if [ $certbot_status -eq 0 ]; then
-    echo "Certificado SSL instalado con éxito"
-else
-    echo "Error al instalar el certificado SSL. Salir."
-    exit 1
-fi
+# Configurar Apache2 con SSL
+sudo tee /etc/apache2/sites-available/nextcloud.conf > /dev/null <<EOL
+<VirtualHost *:80>
+    ServerName agoranube.ddns.net
+    Redirect permanent / https://agoranube.ddns.net/
+</VirtualHost>
 
-# Configurar redirección HTTP a HTTPS en Apache2
-echo -e "<VirtualHost *:80>\n\tServerName agoranube.ddns.net\n\tRedirect permanent / https://agoranube.ddns.net/\n</VirtualHost>" | sudo tee /etc/apache2/sites-available/000-default.conf > /dev/null
+<VirtualHost *:443>
+    ServerName agoranube.ddns.net
+    DocumentRoot /var/www/html/nextcloud
+
+    SSLEngine on
+    SSLCertificateFile /etc/apache2/ssl/nextcloud.crt
+    SSLCertificateKeyFile /etc/apache2/ssl/nextcloud.key
+
+    <Directory /var/www/html/nextcloud/>
+        Options +FollowSymlinks
+        AllowOverride All
+    </Directory>
+
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOL
+
+# Habilitar el sitio
+sudo a2ensite nextcloud
 
 # Reiniciar Apache2
 sudo systemctl restart apache2
@@ -63,10 +89,10 @@ sudo apt install -y nextcloud
 
 # Configurar la base de datos para Nextcloud
 sudo mysql_secure_installation
-sudo mysql -u root -p -e "CREATE DATABASE nextcloud;"
-sudo mysql -u root -p -e "CREATE USER 'nextcloud'@'localhost' IDENTIFIED BY '$db_password';"
-sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost';"
-sudo mysql -u root -p -e "FLUSH PRIVILEGES;"
+sudo mysql -u root -p"$db_password" -e "CREATE DATABASE IF NOT EXISTS nextcloud;"
+sudo mysql -u root -p"$db_password" -e "CREATE USER IF NOT EXISTS 'nextcloud'@'localhost' IDENTIFIED BY '$db_password';"
+sudo mysql -u root -p"$db_password" -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'localhost' WITH GRANT OPTION;"
+sudo mysql -u root -p"$db_password" -e "FLUSH PRIVILEGES;"
 
 # Configurar Nextcloud
 sudo nextcloud.occ maintenance:install --database "mysql" --database-name "nextcloud" --database-user "nextcloud" --database-pass "$db_password"
@@ -84,18 +110,35 @@ echo "Puedes acceder a Nextcloud en: https://agoranube.ddns.net/"
 # Instalar seguridad adicional
 sudo apt install -y fail2ban
 
-# Configuración de seguridad de Apache
-echo -e "ServerTokens Prod\nServerSignature Off" | sudo tee -a /etc/apache2/conf-available/security.conf > /dev/null
-sudo a2enconf security
-
 # Configuración de seguridad de PHP
 sudo sed -i 's/expose_php = On/expose_php = Off/' /etc/php/7.*/apache2/php.ini
 
-# Reiniciar Apache2 después de la configuración de seguridad
+# Reforzar seguridad de MySQL
+sudo tee /etc/mysql/mariadb.conf.d/99-nextcloud.cnf > /dev/null <<EOL
+[mysqld]
+bind-address = 127.0.0.1
+EOL
+
+# Reiniciar servicios después de la configuración de seguridad
 sudo systemctl restart apache2
+sudo systemctl restart mysql
 
 # Monitorización de registros de seguridad
 sudo apt install -y logwatch
 
+# Mostrar los servicios y paquetes instalados
+echo -e "\nServicios y paquetes instalados:"
+echo "---------------------------------"
+echo "1. Apache2"
+echo "2. MariaDB Server"
+echo "3. Nextcloud"
+echo "4. Fail2Ban"
+echo "5. Logwatch"
+
+# Lista de paquetes instalados
+installed_packages=$(dpkg -l | grep ^ii | awk '{print $2}')
+echo -e "\nPaquetes instalados:"
+echo "$installed_packages"
+
 # Mensaje final
-echo "La instalación y configuración de Nextcloud se ha completado con éxito."
+echo -e "\nLa instalación y configuración de Nextcloud se ha completado con éxito."
